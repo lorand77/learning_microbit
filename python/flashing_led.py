22b41ea3f67b9a04e8b283335ceaac17b0f07e83
lorand <lorand77@users.noreply.github.com>
2025-12-24 11:21:42 +0100

    Refactor delay handling and update LED display logic

--- python/flashing_led.py
+++ python/flashing_led.py
@@ -1,20 +1,42 @@
-
 from microbit import *
+import math
 
-time_interval = 500.0
+delay = 500.0
 
+def update_delay():
+    global delay
+    if button_a.was_pressed():
+        delay *= 2
+        if delay > 4000:
+            delay = 4000
+        bar_delay()
+    if button_b.was_pressed():
+        delay /= 2
+        if delay < 125/8:
+            delay = 125/8
+        bar_delay()
+
+def bar_delay():
+    level = int(math.log(delay/(125/8))/math.log(2))
+    for i in range(9):
+        display.set_pixel(i%5, i//5, 0)
+    for i in range(level+1):
+        if i < 5:
+            display.set_pixel(i, 0, 9)
+        else:
+            display.set_pixel(i-5, 1, 9)
+    print(delay)
+
+bar_delay()
 while True:
     pin0.write_digital(1)
-    display.set_pixel(2,2,9)
-    sleep(time_interval)
+    display.set_pixel(2,3,0)
+    display.set_pixel(2,4,9)
+    sleep(delay)
+    update_delay()
+    
     pin0.write_digital(0)
-    display.set_pixel(2,2,0)
-    sleep(time_interval)
-    if button_a.was_pressed():
-        time_interval *=2
-    if button_b.was_pressed():
-        time_interval /=2
-    if time_interval > 2000:
-        time_interval = 2000
-    elif time_interval < 125/2:
-        time_interval = 125/2
+    display.set_pixel(2,3,9)
+    display.set_pixel(2,4,0)
+    sleep(delay)
+    update_delay()
