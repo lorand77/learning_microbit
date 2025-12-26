d8a2b2da32a90bec2695d27836d4173586348e05
lorand <lorand77@users.noreply.github.com>
2025-12-26 09:18:28 +0100

    Refactor CUTEBOT class methods into standalone functions for improved readability

--- cutebot/python/test.py
+++ cutebot/python/test.py
@@ -1,43 +1,23 @@
 from microbit import *
 
 CUTEBOT_ADDR = 0x10
-left = 0x04
-right = 0x08
+i2c.init()
 
+def set_motors_speed(left_wheel_speed, right_wheel_speed):
+    left_direction = 0x02 if left_wheel_speed > 0 else 0x01
+    right_direction = 0x02 if right_wheel_speed > 0 else 0x01
+    left_wheel_speed = left_wheel_speed if left_wheel_speed > 0 else left_wheel_speed * -1
+    right_wheel_speed = right_wheel_speed if right_wheel_speed > 0 else right_wheel_speed * -1
+    i2c.write(CUTEBOT_ADDR, bytearray([0x01, left_direction, left_wheel_speed, 0]))
+    i2c.write(CUTEBOT_ADDR, bytearray([0x02, right_direction, right_wheel_speed, 0]))
 
-class CUTEBOT(object):
-    def __init__(self):
-        i2c.init()
-        self.__pinL = pin13
-        self.__pinR = pin14
-        self.__pinL.set_pull(self.__pinL.PULL_UP)
-        self.__pinR.set_pull(self.__pinR.PULL_UP)
-
-    def set_motors_speed(self, left_wheel_speed: int, right_wheel_speed: int):
-        if left_wheel_speed > 100 or left_wheel_speed < -100:
-            raise ValueError('speed error,-100~100')
-        if right_wheel_speed > 100 or right_wheel_speed < -100:
-            raise ValueError('speed error,-100~100')
-        left_direction = 0x02 if left_wheel_speed > 0 else 0x01
-        right_direction = 0x02 if right_wheel_speed > 0 else 0x01
-        left_wheel_speed = left_wheel_speed if left_wheel_speed > 0 else left_wheel_speed * -1
-        right_wheel_speed = right_wheel_speed if right_wheel_speed > 0 else right_wheel_speed * -1
-        i2c.write(CUTEBOT_ADDR, bytearray(
-            [0x01, left_direction, left_wheel_speed, 0]))
-        i2c.write(CUTEBOT_ADDR, bytearray(
-            [0x02, right_direction, right_wheel_speed, 0]))
-
-    def set_car_light(self, light: int, R: int, G: int, B: int):
-        if R > 255 or G > 255 or B > 255:
-            raise ValueError('RGB is error')
-        i2c.write(CUTEBOT_ADDR, bytearray([light, R, G, B]))
+def set_car_light(light, R, G, B):
+    light_bytecode = 0x04 if light == "left" else 0x08
+    i2c.write(CUTEBOT_ADDR, bytearray([light_bytecode, R, G, B]))
 
 
 if __name__ == '__main__':
-    ct = CUTEBOT()
-
-    ct.set_motors_speed(30, 50)
-    ct.set_car_light(left, 30, 30, 90)
-
+    set_motors_speed(30, 50)
+    set_car_light("left", 30, 30, 90)
     sleep(3000)
-    ct.set_motors_speed(0, 0)
+    set_motors_speed(0, 0)
