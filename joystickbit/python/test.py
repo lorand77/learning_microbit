68c364acaaed409edb51f65e93608c91dbba2004
lorand <lorand77@users.noreply.github.com>
2025-12-26 10:55:59 +0100

    joystick

--- joystickbit/python/test.py
+++ joystickbit/python/test.py
@@ -1,12 +1,17 @@
 from microbit import *
+import sys
 
 pin14.set_pull(pin14.PULL_UP)
 
 while True:
-    x = pin14.read_digital()
-    if x == 0:
+    b = pin14.read_digital()
+    if b == 0:
         display.set_pixel(0, 0, 9)
     else:
         display.set_pixel(0, 0, 0)
-    print(x)
-    sleep(300)
+
+    x = pin1.read_analog()
+    y = pin2.read_analog()
+
+    print("X:", x, " Y:", y, "Btn:", b)
+    sleep(500)
